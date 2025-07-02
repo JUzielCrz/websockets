7984f2002f8cb6ae1dc2935393f4624a6ec42cb0
JeftAC <uj.alonso@ebc.edu.mx>
2025-07-02 16:48:07 -0600

    Initial configurate

---
+++ backend/src/main/groovy/com/exercises/websockets/WebsocketsApplication.groovy
@@ -0,0 +1,13 @@
+package com.exercises.websockets
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class WebsocketsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(WebsocketsApplication, args)
+	}
+
+}
